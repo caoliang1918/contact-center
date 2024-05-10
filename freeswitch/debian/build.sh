804fa5f15accd4a44f94bc92ba9c47e03f6b34a6
caoliang1918@gmail.com <caoliang1918@gmail.com>
2024-05-10 15:53:51 +0800

    freeswitch:1.10.11

--- freeswitch/debian/build.sh
+++ freeswitch/debian/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build --no-cache -t registry.cn-hangzhou.aliyuncs.com/voice9-x86/freeswitch-debian:1.10.10  .
+docker build  -t registry.cn-hangzhou.aliyuncs.com/voice9-x86/freeswitch-debian:1.10.11 .
 
 
 
